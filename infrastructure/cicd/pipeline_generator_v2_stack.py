29c372d6807033ca135afcc3286e576a08f30dfc
wolfgangunger <wolfgang.unger@gmx.de>
2023-07-18 14:59:20 -0300

    feature branch

--- infrastructure/cicd/pipeline_generator_v2_stack.py
+++ infrastructure/cicd/pipeline_generator_v2_stack.py
@@ -139,7 +139,7 @@ class PipelineGeneratorStack(Stack):
             ),
             commands=[
                 "pip install -r requirements.txt",
-                "pip install -r requirements_dev.txt",
+                "pip install -r requirements-dev.txt",
                 #"pytest -vvvv -s generic/infrastructure/tests",
             ],
         )
