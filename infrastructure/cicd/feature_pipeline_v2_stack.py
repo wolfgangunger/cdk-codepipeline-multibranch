165508a7e2ba524e42caef327be2350dc035277b
wolfgangunger <wolfgang.unger@gmx.de>
2023-07-19 14:57:12 -0300

    pipelinesuper class

--- infrastructure/cicd/feature_pipeline_v2_stack.py
+++ infrastructure/cicd/feature_pipeline_v2_stack.py
@@ -14,9 +14,9 @@ from infrastructure.cicd.app_deploy import AppDeploy, AppDeployBootstrap
 from infrastructure.cicd.toolchain_deploy import ToolchainDeploy
 from infrastructure.cicd.toolchain_deploy import ToolchainDeploy
 
+from generic.infrastructure.cicd.pipeline_stack import PipelineStack
 
-
-class FeaturePipelineStack(Stack):
+class FeaturePipelineStack(PipelineStack):
     def __init__(
         self,
         scope: Construct,
@@ -26,129 +26,38 @@ class FeaturePipelineStack(Stack):
         config: dict = None,
         **kwargs,
     ):
-        super().__init__(scope, id, **kwargs)
-
-        branch_name = feature_branch_name
-
-        codestar_connection_arn = config.get("connection_arn")
-        repo_owner = config.get("owner")
-        repo = config.get("repo")
-
-        source_artifact = codepipeline.Artifact()
-        cloud_assembly_artifact = codepipeline.Artifact()
-
-        accounts = config.get("accounts")
-        region: str = accounts["tooling"]["region"]
-        dev_account: str = accounts["dev"]["account"]
-        qa_account: str = accounts["qa"]["account"]
-        prod_account: str = accounts["prod"]["account"]
-        toolchain_account: str = accounts["tooling"]["account"]
-
-        synth_dev_account_role_arn = (
-            f"arn:aws:iam::{dev_account}:role/codebuild-role-from-toolchain-account"
+        
+        super().__init__(
+            scope,
+            id,
+            development_pipeline=True,
+            config=config,
+            **kwargs,
         )
+    
+# this is the feature branch pipeline 
+# it got the same superclass as the normal pipeline, thus same actions to synth, deploy and test
+# you can setup a different feature pipline clss instead, for example without deployment or with less testes or whatever
+# you can also create different pipelines with the pipeline generator, for example faster pipelines with less methods/actions
+# or full piplines
 
-        synth_qa_account_role_arn = (
-            f"arn:aws:iam::{qa_account}:role/codebuild-role-from-toolchain-account"
-        )
-        synth_prod_account_role_arn = (
-            f"arn:aws:iam::{prod_account}:role/codebuild-role-from-toolchain-account"
-        )
-
-        # creating the pipline with  synch action
+    ## overwritten methods
+    ## the source step must be overwritten for branch name
+    def get_connection(
+        self,
+        repo_owner,
+        repo,
+        config,
+        development_pipeline,
+        codestar_connection_arn
+    ):
         git_input = pipelines.CodePipelineSource.connection(
             repo_string=f"{repo_owner}/{repo}",
-            branch=branch_name,
+            branch=self.feature_branch_name,
             connection_arn=codestar_connection_arn,
         )
-
-        synth_step = self.get_sync_step(
-            git_input,
-            synth_dev_account_role_arn,
-            synth_qa_account_role_arn,
-            synth_prod_account_role_arn,
-            feature_pipeline_suffix,
-        )
-
-        pipeline = CodePipeline(
-            self,
-            id,
-            pipeline_name=id,   
-            #pipeline_name="feature-branch-pipeline-template",
-            synth=synth_step,
-            cross_account_keys=True,
-            code_build_defaults=pipelines.CodeBuildOptions(
-                build_environment=BuildEnvironment(
-                    build_image=aws_codebuild.LinuxBuildImage.STANDARD_5_0,
-                    privileged=True,
-                )
-            ),
-        )
-
-        toolchain_app = ToolchainDeploy(
-            self,
-            "toolchain",
-            config=config,
-            env={
-                "account": toolchain_account,
-                "region": region,
-            },
-        )
-
-        toolchain_stage = pipeline.add_stage(toolchain_app)
-
-        ## add unit tests for toolchain to run before toolchain
-        infrastructure_unit_tests = self.get_infrastructure_unit_tests(git_input)
-        if infrastructure_unit_tests != None:
-            toolchain_stage.add_pre(infrastructure_unit_tests)
-
-        ##  lambda tests
-        lambda_tests = self.get_lambda_tests(git_input)
-        if lambda_tests != None:
-            toolchain_stage.add_pre(lambda_tests)
-
-
-        # Deploy bootstrap
-        dev_app_bootstrap = AppDeployBootstrap(
-            self,
-            "devbootstrap",
-            config=config,
-            env={
-                "account": dev_account,
-                "region": region,
-            },
-        )
-        dev_bootstrap_stage = pipeline.add_stage(dev_app_bootstrap)
-
-        # Dev deploy
-        dev_app = AppDeploy(
-            self,
-            "dev",
-            config=config,
-            env={
-                "account": dev_account,
-                "region": region,
-            },
-        )
-        ## deploy dev stack
-        dev_stage = pipeline.add_stage(dev_app)
-
-        ## integration tests
-        dev_int_tests = self.get_dev_int_tests(
-            git_input, region, dev_account, synth_dev_account_role_arn
-        )
-        if dev_int_tests != None:
-            dev_stage.add_post(dev_int_tests)
-
-        ## acceptance tests
-        dev_acceptance_tests = self.get_dev_acceptance_tests(
-            git_input, region, dev_account, synth_dev_account_role_arn
-        )
-        if dev_acceptance_tests != None:
-            dev_stage.add_post(dev_acceptance_tests)
-
-
-
+        return git_input
+    ## the synth step must be overwritten to adapt the branch name
     def get_sync_step(
         self,
         git_input,
@@ -194,111 +103,6 @@ class FeaturePipelineStack(Stack):
         ]
         return commands
 
-    def get_infrastructure_unit_tests(self, git_input):
-        infrastructure_unit_tests = pipelines.CodeBuildStep(
-            "UnitTests",
-            input=git_input,
-            commands=self.get_infrastructure_unit_tests_commands(),
-        )
-        return infrastructure_unit_tests
 
-    def get_infrastructure_unit_tests_commands(self) -> list:
-        commands = [
-            "pip install -r requirements.txt && pip install -r requirements-dev.txt",
-            "pytest -vvvv -s generic/infrastructure/tests",
-        ]
-        return commands
 
-    ####
-    def get_lambda_tests(self, git_input):
-        infrastructure_unit_tests = pipelines.CodeBuildStep(
-            "LambdaTests",
-            input=git_input,
-            commands=self.get_lambda_tests_commands(),
-        )
-        return infrastructure_unit_tests
 
-    def get_lambda_tests_commands(self) -> list:
-        commands = [
-            "pip install -r requirements.txt && pip install -r requirements-dev.txt",
-            "pytest -vvvv -s infrastructure/lambdas/tests",
-        ]
-        return commands
-
-    def get_dev_int_tests(self, git_input, region, dev_account, dev_account_role_arn):
-        dev_int_tests = pipelines.CodeBuildStep(
-            "IntegrationTests",
-            input=git_input,
-            build_environment=aws_codebuild.BuildEnvironment(
-                build_image=aws_codebuild.LinuxBuildImage.AMAZON_LINUX_2_3,
-                privileged=True,
-            ),
-            commands=self.get_dev_int_tests_commands(
-                region, dev_account, dev_account_role_arn
-            ),
-            role_policy_statements=[
-                iam.PolicyStatement(
-                    actions=["sts:AssumeRole"],
-                    effect=iam.Effect.ALLOW,
-                    resources=[dev_account_role_arn],
-                )
-            ],
-        )
-        return dev_int_tests
-
-    def get_dev_int_tests_commands(
-        self, region, dev_account, dev_account_role_arn
-    ) -> list:
-        ### setting temp credentials to access stage account
-        commands = [
-            "pip install -r requirements.txt && pip install -r requirements-dev.txt",
-            "docker ps",
-            f"REGION={region}",
-            f"ACCOUNT_ID={dev_account}",
-            f'TEMP_CREDS=$(aws sts assume-role --role-arn {dev_account_role_arn} --role-session-name "integration-test")',
-            "export TEMP_CREDS",
-            'export ACCESS_KEY_ID=$(echo "${TEMP_CREDS}" | jq -r ".Credentials.AccessKeyId")',
-            'export SECRET_ACCESS_KEY_ID=$(echo "${TEMP_CREDS}" | jq -r ".Credentials.SecretAccessKey")',
-            'export TOKEN=$(echo "${TEMP_CREDS}" | jq -r ".Credentials.SessionToken")',
-            'AWS_ACCESS_KEY_ID="${ACCESS_KEY_ID}" AWS_SECRET_ACCESS_KEY="${SECRET_ACCESS_KEY_ID}" AWS_SESSION_TOKEN="${TOKEN}" pytest -vvvv -s tests/integration/',
-        ]
-        return commands
-
-    def get_dev_acceptance_tests(
-        self, git_input, region, dev_account, dev_account_role_arn
-    ):
-        tests = pipelines.CodeBuildStep(
-            "AcceptanceTests",
-            input=git_input,
-            build_environment=aws_codebuild.BuildEnvironment(
-                build_image=aws_codebuild.LinuxBuildImage.AMAZON_LINUX_2_3,
-                privileged=True,
-            ),
-            env={"SKIP_TESTS": "0"},
-            timeout=Duration.minutes(180),
-            commands=self.get_dev_acceptance_tests_commands(region, dev_account),
-            role_policy_statements=[
-                iam.PolicyStatement(
-                    actions=["sts:AssumeRole"],
-                    effect=iam.Effect.ALLOW,
-                    resources=[dev_account_role_arn],
-                ),
-                iam.PolicyStatement(
-                    actions=[
-                        "cloudwatch:PutMetricData",
-                        "cloudwatch:GetMetricData",
-                    ],
-                    effect=iam.Effect.ALLOW,
-                    resources=["*"],
-                ),
-            ],
-        )
-        return tests
-
-    def get_dev_acceptance_tests_commands(self, region, dev_account) -> list:
-        commands = [
-            "pip install -r requirements.txt && pip install -r requirements-dev.txt",
-            "chmod 777 tests/acceptance/tests.sh",
-            f"tests/acceptance/tests.sh {dev_account}",
-        ]
-        return commands
